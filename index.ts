ab5abaf49926cefcb7806e2f652c5b4a03d91053
Shovon Hasan <im.shovonhasan@gmail.com>
2018-01-10 00:17:22 -0500

    Scraper will now execute different scraper depending on URL.

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
-// import { ChanScraper } from './scrapers/ChanScraper';
+import { ChanScraper } from './scrapers/ChanScraper';
 import { ImgurScraper } from './scrapers/ImgurScraper';
 
-process.argv.slice(2).forEach(ImgurScraper.downloadSubredditView);
+process.argv.slice(2).forEach((arg: string) => {
+  if (/imgur/.test(arg) && /\/r\//.test(arg)) {
+    return ImgurScraper.downloadSubredditView(arg);
+  } else if (/4chan/.test(arg)) {
+    return ChanScraper.downloadThread(arg);
+  } else {
+    console.error('Invalid URL.');
+    return;
+  }
+});
