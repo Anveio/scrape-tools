0c896b936276e6786dd90159ab379e664c6d4959
anveio <im.shovonhasan@gmail.com>
2018-03-29 03:40:07 -0400

    Remove unnecessary configurations.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ import { fork } from 'child_process';
 process.argv.slice(2).forEach(url => {
   const childProcess = fork('./src/processes/argvHandler.ts');
   childProcess.send(url);
-  childProcess.on('disconnect', () => `Finished downloading ${url}`);
 });
